75fa6c62e8feb852192b0d371f8daca2815ac4c2
sushant babu prasai <sushantprasaii7@gmail.com>
2023-10-20 13:13:08 +0545

    sidebar added

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,9 +1,9 @@
 import Navbar from "./navbar";
-
+import Sidebar from "./sidebar";
 export default function Home() {
   return (
     <div>
-
+      <Sidebar/>
       <Navbar/>
       <h1 className=" text-center text-2xl font-semibold pt-3 my-2 ">Ecommerce with UseContext and useReducer</h1>
     </div>
--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,7 +1,53 @@
-export default function Sidebar() {
+import { useState } from "react";
+import {
+  ContainerOutlined,
+  DesktopOutlined,
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  PieChartOutlined,
+} from "@ant-design/icons";
+
+import { Button, Menu } from "antd";
+function getItem(label, key, icon, children, type) {
+  return {
+    key,
+    icon,
+    children,
+    label,
+    type,
+  };
+}
+const items = [
+  getItem("Option 1", "1", <PieChartOutlined />),
+  getItem("Option 2", "2", <DesktopOutlined />),
+  getItem("Option 3", "3", <DesktopOutlined />),
+  getItem("Option 4", "4", <ContainerOutlined />),
+  getItem("Option 5", "5", <ContainerOutlined />),
+];
+const Sidebar = () => {
+  const [collapsed, setCollapsed] = useState(false);
+  const toggleCollapsed = () => {
+    setCollapsed(!collapsed);
+  };
   return (
-    <div>
-      <h2>I am side bar</h2>
+    <div className="w-[16rem]">
+      <Button
+        onClick={toggleCollapsed}
+        className=" text-[1.4rem] mb-4 border-none"
+      >
+        {collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+      </Button>
+
+      <Menu
+        defaultSelectedKeys={["1"]}
+        defaultOpenKeys={["sub1"]}
+        mode="inline"
+        theme="dark"
+        inlineCollapsed={collapsed}
+        items={items}
+        className="p-3 my-2"
+      />
     </div>
   );
-}
+};
+export default Sidebar;
